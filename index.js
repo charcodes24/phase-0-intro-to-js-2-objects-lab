a42c3b4ca1e34b37c6408e62669ff1c6baedcff4
Charlene Johnson <charcodes24@gmail.com>
2021-03-19 16:48:06 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name : 'Charlene Johnson', 
+    streetAddress : '2146 S Johnson Ct, Visalia, CA, 93277', 
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newObject = Object.assign({}, employee);
+    //console.log(newObject, key);
+    delete newObject[key];
+    return newObject;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
